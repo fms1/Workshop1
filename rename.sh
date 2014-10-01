4c5a91c0e70da8e5e110e48a14de9e7b7ddbfd5b
FathiS <salem@egr.msu.edu>
2014-10-01 15:57:39 -0400

    hello

---
+++ rename.sh
@@ -0,0 +1,7 @@
+for filename in chem/*.dpb
+do
+	basicFilename=$(basename $filename .pdb)
+	foldername=$(dirname $filename .pdb)
+	foldername=$(dirname $filename)
+	echo "mv $filename $foldername/$basicFIlename.txt"
+done
